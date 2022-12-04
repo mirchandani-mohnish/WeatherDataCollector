09ad394628c560311059c95c11b59ea6730f14c8
Mohnish <mirchandani.mohnish03@gmail.com>
2022-12-04 14:10:22 +0530

    Update: js file added for serial read

---
+++ Lora_Receiver_Node/Lora_Receiver_Client.js
@@ -0,0 +1,15 @@
+const {SerialPort} = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+
+
+const port = new SerialPort({path: '/dev/COM9', baudRate: 9600 });
+const parser = port.pipe(new ReadlineParser({ delimiter: '//' }));
+
+// Read the port data
+port.on("open", () => {
+  console.log('serial port open');
+});
+
+parser.on('data', data =>{
+  console.log('got word from arduino:', data);
+});
