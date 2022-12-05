af27a85b14b185a8d61615862cca1ece68f7cc94
Mohnish <mirchandani.mohnish03@gmail.com>
2022-12-05 11:52:31 +0530

    Update: Client working

--- Lora_Receiver_Node/Lora_Receiver_Client.js
+++ Lora_Receiver_Node/Lora_Receiver_Client.js
@@ -3,19 +3,20 @@ const { ReadlineParser } = require('@serialport/parser-readline');
 const axios = require("axios");
 
 const port = new SerialPort({path: 'COM9', baudRate: 9600 });
-const parser = port.pipe(new ReadlineParser({ delimiter: '//' }));
+const parser = port.pipe(new ReadlineParser());
 
 
-const postDataRepeat = setTimeout(postData, 1000);
+// const postDataRepeat = setTimeout(postData, 1000);
 
-const postData = () => {
+// const postData = () => {
+
 port.on("open", () => {
   console.log('serial port open');
 });
 
 parser.on('data', (data) =>{
   console.log('got word from arduino:', data);
-  axios.post("http://localhost:3000/data").then(() => console.log("posted weather data")).catch((e) => console.log(e));
+//   axios.post("http://localhost:3000/data").then(() => console.log("posted weather data")).catch((e) => console.log(e));
 });
 
-}
+// }
