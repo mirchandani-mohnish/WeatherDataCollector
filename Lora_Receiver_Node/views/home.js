7cc0cf5422a67f88b9bd43fe75dbea3d18adcfeb
Akshil Thumar <akshilvthumar@gmail.com>
2022-12-06 16:36:59 +0530

    chart

--- Lora_Receiver_Node/views/home.js
+++ Lora_Receiver_Node/views/home.js
@@ -1,21 +1,21 @@
-function renderChart(data, labels) {
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'This week',
-                data: data,
-            }]
-        },
-    });
-}
-
-$("#renderBtn").click(
-    function () {
-        data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
-        labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-        renderChart(data, labels);
-    }
-);
+let ctx = document.getElementById('chart').getContext('2d');
+let chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['2020/Q1', '2020/Q2', '2020/Q3', '2020/Q4'],
+    datasets: [
+      {
+        label: 'Gross volume ($)',
+        backgroundColor: '#79AEC8',
+        borderColor: '#417690',
+        data: [26900, 28700, 27300, 29200],
+      },
+    ],
+  },
+  options: {
+    title: {
+      text: 'Gross Volume in 2020',
+      display: true,
+    },
+  },
+});
